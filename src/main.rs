1a304d618c01005ccbabc77ec7ad6c7bd4e0d8bb
shaokun <605416017@qq.com>
2023-08-08 18:09:29 +0800

    增加bytecode的disassembly的功能

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use hex::{FromHex, FromHexError};
 use serde::{Deserialize, Deserializer, Serialize};
+use std::env;
+use serde_json::{json, Value};
 
 #[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq)]
 pub struct Any {
@@ -72,10 +74,9 @@ impl AccountAddress {
 
 
 fn bsc_hex() {
-    use std::env;
     let args: Vec<String> = env::args().collect();
     // skip the first argument, its the name
-    let json_hex = args.get(1).unwrap();
+    let json_hex = args.get(2).unwrap();
     let json_str = String::from_utf8(hex::decode(json_hex).unwrap()).unwrap();
     let json: PackageMetadata = serde_json::from_str(&json_str).unwrap();
     let res = bcs::to_bytes(&json).unwrap();
@@ -83,5 +84,25 @@ fn bsc_hex() {
     println!("{}", ret);
 }
 
-fn main() { bsc_hex() }
+
+fn disassemble() {
+    let args: Vec<String> = env::args().collect();
+    let bytecode_hex = args.get(2).unwrap();
+    let code = hex::decode(bytecode_hex).unwrap();
+    // let code = hex::decode("a11ceb0b060000000b01000202020403060f05150c072137085820067822109a01260ac001050cc501470d8c0202000000010800000200010000030200000004030000000103010c01060c010708000764656d6f31303807436f756e7465720b6765745f636f756e74657208696e6372656173650b696e69745f6d6f64756c6505636f756e74a272d39841bac0be1e8a8f5f4ab3185be67527cd7f5d8f3973df6140961299f90520a272d39841bac0be1e8a8f5f4ab3185be67527cd7f5d8f3973df6140961299f9126170746f733a3a6d657461646174615f7631120000010b6765745f636f756e74657201010000020105030001000100000507002b00100014020101040100040c07002a000c010a01100014060100000000000000160b010f0015020200000000050b0006010000000000000012002d0002000000").unwrap();
+    let result = move_binary_format::CompiledModule::deserialize(&code).unwrap();
+    let res = format!("{:#?}", result);
+    println!("{}", res);
+}
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let cmd = args.get(1).unwrap();
+    match cmd.as_str() {
+        "bcs" => bsc_hex(),
+        "disassemble" => disassemble(),
+        _ => {}
+    }
+}
 
